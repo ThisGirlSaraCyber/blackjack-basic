0231f76a69db9dfe498f5e90d4957b170e5379af
thisgirlsara <thisgirlsara.cyber@gmail.com>
2020-05-01 22:39:27 -0500

    add: blackjack-basic

---
+++ src/views/Blackjack-basic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="blackjack-basic">
+    <Game msg="Welcome to Your Game"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Game from '@/components/Game.vue'
+
+export default {
+  name: 'Blackjack-basic',
+  components: {
+    Game
+  }
+}
+</script>
