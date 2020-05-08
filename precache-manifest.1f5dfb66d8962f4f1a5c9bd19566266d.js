14897c14d8049523829bf78a82e9c957f74c2f39
thisgirlsara <thisgirlsara.cyber@gmail.com>
2020-05-07 23:58:06 -0500

    deploy

---
+++ precache-manifest.1f5dfb66d8962f4f1a5c9bd19566266d.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14210e18cf900d0fd49",
+    "url": "/blackjack-basic/css/about.715d7f3b.css"
+  },
+  {
+    "revision": "d2d613965934e98c8eef",
+    "url": "/blackjack-basic/css/app.878ec131.css"
+  },
+  {
+    "revision": "b0d1597d3ef8657de425b02261017ee1",
+    "url": "/blackjack-basic/img/BJA_Basic_Strategy.b0d1597d.jpg"
+  },
+  {
+    "revision": "2538a0112234c112967405b34f47f060",
+    "url": "/blackjack-basic/img/card_back.2538a011.png"
+  },
+  {
+    "revision": "dbc86bbac6974eab23271c2ed6168b01",
+    "url": "/blackjack-basic/img/card_club_01.dbc86bba.png"
+  },
+  {
+    "revision": "9ad368695e7128ae6da0c402728ca40b",
+    "url": "/blackjack-basic/img/card_club_02.9ad36869.png"
+  },
+  {
+    "revision": "2b2664d0df7ed9f3262263afc77b530a",
+    "url": "/blackjack-basic/img/card_club_03.2b2664d0.png"
+  },
+  {
+    "revision": "e7e55c8c8c2959ae58e5e1f3769bdbf3",
+    "url": "/blackjack-basic/img/card_club_04.e7e55c8c.png"
+  },
+  {
+    "revision": "fd216baab5a83a1467f2fb7f56604ad1",
+    "url": "/blackjack-basic/img/card_club_05.fd216baa.png"
+  },
+  {
+    "revision": "305d9eb37746a1dce4982cf7dcfd6e2e",
+    "url": "/blackjack-basic/img/card_club_06.305d9eb3.png"
+  },
+  {
+    "revision": "f90c1255b6406efb2a7de0f6c5033ab3",
+    "url": "/blackjack-basic/img/card_club_07.f90c1255.png"
+  },
+  {
+    "revision": "505533bb08be88b67ca9acaa4518cf18",
+    "url": "/blackjack-basic/img/card_club_08.505533bb.png"
+  },
+  {
+    "revision": "fe8f70dd492c05aa94d731d02632d782",
+    "url": "/blackjack-basic/img/card_club_09.fe8f70dd.png"
+  },
+  {
+    "revision": "50fe0fb70bbbec1fbab672d068421ca1",
+    "url": "/blackjack-basic/img/card_club_10.50fe0fb7.png"
+  },
+  {
+    "revision": "edf82d320fb70977b55fdfd3f928a888",
+    "url": "/blackjack-basic/img/card_club_11.edf82d32.png"
+  },
+  {
+    "revision": "836db14388bc3113b5db8ab8b69293d0",
+    "url": "/blackjack-basic/img/card_club_12.836db143.png"
+  },
+  {
+    "revision": "a57ce791647c0feec594220e6212a729",
+    "url": "/blackjack-basic/img/card_club_13.a57ce791.png"
+  },
+  {
+    "revision": "4524a4ed00c3a8c2c65f3115ae5839a3",
+    "url": "/blackjack-basic/img/card_diamond_01.4524a4ed.png"
+  },
+  {
+    "revision": "031c6a74c08c5e3dc5b22f626e49184e",
+    "url": "/blackjack-basic/img/card_diamond_02.031c6a74.png"
+  },
+  {
+    "revision": "718674d622baaf50971ff914fda459e9",
+    "url": "/blackjack-basic/img/card_diamond_03.718674d6.png"
+  },
+  {
+    "revision": "b2a0bd31b7b485732967d4445753fbeb",
+    "url": "/blackjack-basic/img/card_diamond_04.b2a0bd31.png"
+  },
+  {
+    "revision": "211ce5a5f3cc856c4023dcb34f407bcc",
+    "url": "/blackjack-basic/img/card_diamond_05.211ce5a5.png"
+  },
+  {
+    "revision": "8de79ed0038245adca5c1cf123168e4a",
+    "url": "/blackjack-basic/img/card_diamond_06.8de79ed0.png"
+  },
+  {
+    "revision": "a8416aedd3fe7b93ccd4b35a6803a7f9",
+    "url": "/blackjack-basic/img/card_diamond_07.a8416aed.png"
+  },
+  {
+    "revision": "9448bb3a6df9706c02d964ad7b53620d",
+    "url": "/blackjack-basic/img/card_diamond_08.9448bb3a.png"
+  },
+  {
+    "revision": "f72b4a24fd067f00ec163ab3c58d1afc",
+    "url": "/blackjack-basic/img/card_diamond_09.f72b4a24.png"
+  },
+  {
+    "revision": "20511720506454de217a2ad4f45a0e56",
+    "url": "/blackjack-basic/img/card_diamond_10.20511720.png"
+  },
+  {
+    "revision": "81050dbed4d70a9a9d992b1e6dcc75b0",
+    "url": "/blackjack-basic/img/card_diamond_11.81050dbe.png"
+  },
+  {
+    "revision": "7b8e8481e4281a3ea0280854543607b9",
+    "url": "/blackjack-basic/img/card_diamond_12.7b8e8481.png"
+  },
+  {
+    "revision": "59dd1d395ffc47597d7090058b84ca33",
+    "url": "/blackjack-basic/img/card_diamond_13.59dd1d39.png"
+  },
+  {
+    "revision": "e34c418506f56702c4605c52f1af8988",
+    "url": "/blackjack-basic/img/card_heart_01.e34c4185.png"
+  },
+  {
+    "revision": "772994f6924d757d34093da21d05449e",
+    "url": "/blackjack-basic/img/card_heart_02.772994f6.png"
+  },
+  {
+    "revision": "885e91c0337d12bb4488a360728c8559",
+    "url": "/blackjack-basic/img/card_heart_03.885e91c0.png"
+  },
+  {
+    "revision": "905180774235c90bd4abed25553e97a9",
+    "url": "/blackjack-basic/img/card_heart_04.90518077.png"
+  },
+  {
+    "revision": "64b4b1dd81a51442beca9ab32c9e3be1",
+    "url": "/blackjack-basic/img/card_heart_05.64b4b1dd.png"
+  },
+  {
+    "revision": "c577b690ee8dbd65b6895bca98a31e8c",
+    "url": "/blackjack-basic/img/card_heart_06.c577b690.png"
+  },
+  {
+    "revision": "c58ca3dac8f4f87dd2d3abf53adb5aa1",
+    "url": "/blackjack-basic/img/card_heart_07.c58ca3da.png"
+  },
+  {
+    "revision": "76111f48f73a79d947ebdaadb90eeb41",
+    "url": "/blackjack-basic/img/card_heart_08.76111f48.png"
+  },
+  {
+    "revision": "806e505398c79e277af3e71462bca56e",
+    "url": "/blackjack-basic/img/card_heart_09.806e5053.png"
+  },
+  {
+    "revision": "50c409e86bab59717466bef9b1928aac",
+    "url": "/blackjack-basic/img/card_heart_10.50c409e8.png"
+  },
+  {
+    "revision": "ad8fc4efb04f35e8d98ce3008aecc4a2",
+    "url": "/blackjack-basic/img/card_heart_11.ad8fc4ef.png"
+  },
+  {
+    "revision": "61e1cee775a470b2f42809c57a138c6c",
+    "url": "/blackjack-basic/img/card_heart_12.61e1cee7.png"
+  },
+  {
+    "revision": "52ba8e6d8f21c16238c2d88621c9f503",
+    "url": "/blackjack-basic/img/card_heart_13.52ba8e6d.png"
+  },
+  {
+    "revision": "8a8677c34918f4f1e09bd6daf8c482e0",
+    "url": "/blackjack-basic/img/card_spade_01.8a8677c3.png"
+  },
+  {
+    "revision": "42b07b4c5a46b3504cf3bf82e255acaa",
+    "url": "/blackjack-basic/img/card_spade_02.42b07b4c.png"
+  },
+  {
+    "revision": "0506e83a7ca2358c493b01bd105eb5fa",
+    "url": "/blackjack-basic/img/card_spade_03.0506e83a.png"
+  },
+  {
+    "revision": "85a3a95a30bf0ca2b3da861a37909da6",
+    "url": "/blackjack-basic/img/card_spade_04.85a3a95a.png"
+  },
+  {
+    "revision": "1411755f3de8adb87ae7391c28ba683e",
+    "url": "/blackjack-basic/img/card_spade_05.1411755f.png"
+  },
+  {
+    "revision": "e7b12f8fb117688ca2a40529027cb9c1",
+    "url": "/blackjack-basic/img/card_spade_06.e7b12f8f.png"
+  },
+  {
+    "revision": "ed3ef00af8d7b6183773fb413f44d1f8",
+    "url": "/blackjack-basic/img/card_spade_07.ed3ef00a.png"
+  },
+  {
+    "revision": "6c2353b9f991cd2637e8659cd3e89b8c",
+    "url": "/blackjack-basic/img/card_spade_08.6c2353b9.png"
+  },
+  {
+    "revision": "a2e92aa4ae24ee364432f2dc60fedcf4",
+    "url": "/blackjack-basic/img/card_spade_09.a2e92aa4.png"
+  },
+  {
+    "revision": "465c4691aebcd1d1b7aa634a1b6b4d7a",
+    "url": "/blackjack-basic/img/card_spade_10.465c4691.png"
+  },
+  {
+    "revision": "8617ac69018310055a55df5eb0b88a83",
+    "url": "/blackjack-basic/img/card_spade_11.8617ac69.png"
+  },
+  {
+    "revision": "71cd9d267d7d172fff7203dc0380d70b",
+    "url": "/blackjack-basic/img/card_spade_12.71cd9d26.png"
+  },
+  {
+    "revision": "40eee4ac7929f4bccc245b05779eba7c",
+    "url": "/blackjack-basic/img/card_spade_13.40eee4ac.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/blackjack-basic/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0bdcd658773817af9c1849a0a85fa745",
+    "url": "/blackjack-basic/index.html"
+  },
+  {
+    "revision": "c14210e18cf900d0fd49",
+    "url": "/blackjack-basic/js/about.2c4eb60b.js"
+  },
+  {
+    "revision": "d2d613965934e98c8eef",
+    "url": "/blackjack-basic/js/app.56652fd6.js"
+  },
+  {
+    "revision": "b6d9c3667b15beebaabe",
+    "url": "/blackjack-basic/js/chunk-vendors.e6aa2577.js"
+  },
+  {
+    "revision": "f99ff5a67e4697ca6fe4cbda06f7e42e",
+    "url": "/blackjack-basic/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blackjack-basic/robots.txt"
+  }
+]);
